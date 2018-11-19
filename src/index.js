b6000fc807d1f2e02ba6c6c069ea29390fb489f1
sofianeyra <safirokpoper@gmail.com>
2018-11-19 17:33:55 -0500

    adding function to cipher

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+const desplazamiento = document.getElementById("desplazamiento");
+const texto = document.getElementById("texto");
+const textoCifrado = document.getElementById("cifrado");
 
-function cCifrado(){document.getElementById('cCifrado').style.display='none'}
-function cDescifrado(){document.getElementById('cDescifrado').style.display='none'}
+function cifrado(){
+const textoIngresado = texto.value;
+textoCifrado.value = textoIngresado.split('').map(c=>{
+let mayus = (c === c.toUpperCase()) ? true : false;
+let valorEntero = c.toLowerCase().charCodeAt(0);
+	if(valorEntero >= 97 && valorEntero <= 122)
+{
+const valorDesplazamiento = parseInt(desplazamiento.value);
+  if(valorEntero + valorDesplazamiento > 122)
+valorEntero = 97 + (valorEntero - 122) + valorDesplazamiento - 1;
+  else
+valorEntero = valorEntero + valorDesplazamiento;
+}
+let cifrado = String.fromCharCode(valorEntero);
+	return mayus ? cifrado.toUpperCase() : cifrado;
+}).join('');
+}
+texto.addEventListener("keyup",cifrado);
+desplazamiento.addEventListener("change", cifrado);
