32001baef8dbd9a54893deb2bf4156977e299847
sofianeyra <safirokpoper@gmail.com>
2018-11-22 21:15:33 -0500

    Function Code and Encode

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
+//declaracion de variables y arreglos vacios
 
-function cCifrado(){document.getElementById('cCifrado').style.display='none'}
-function cDescifrado(){document.getElementById('cDescifrado').style.display='none'}
+let message;
+let string = [];
+let code = [];
+let n;
+let j;
+let newCode = [];
+//FunciónCifrado
+function encodeText()
+{
+message = document.getElementById("fraseInicial").value; //guarda el valor del mensaje ingresado
+n = document.getElementById("key").value;
+//n guarda el desplazamiento del cifrado
+for(j = 0; j < message.length; j++){
+  string.push((message.charCodeAt(j))-65+n%26+65);
+  code.push(String.fromCharCode(string[j]));
+}
+  newCode = code.join("");
+	//muestra el mensaje ya cifrado o descifrado
+  document.getElementById("descifrado").innerHTML = (newCode.toUpperCase());
+
+ /* console.log(string); muestra codigo ASCII
+ console.log(code); muestra mensaje cifrado */
+  //vacia las cadenas
+  string = [];
+  code = [];
+}
+//FunciónDescifrado
+function uncodeText()
+{
+  message = document.getElementById("fraseInicial").value;
+  n = document.getElementById("key").value; //n guarda el numero el desplazamiento del descifrado
+  for(j = 0; j < message.length; j++){
+  string.push((message.charCodeAt(j))+65-n%26-65);
+  code.push(String.fromCharCode(string[j]));
+}
+  newCode = code.join("");
+ //muestra el mensaje descifrado
+	document.getElementById("descifrado").innerHTML = (newCode.toUpperCase());
+
+ /* console.log(string); muestra codigo ASCII
+ console.log(code); muestra mensaje descifrado */
+  string = []; 
+  code = [];
+}
